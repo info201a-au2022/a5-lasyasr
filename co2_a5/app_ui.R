47a1d306c3eb04c523dc9e0a836f7d0ce1bccec3
lasyasr <lasya19@uw.edu>
2022-12-08 17:50:46 -0800

    Final push- no more changes

--- co2_a5/app_ui.R
+++ co2_a5/app_ui.R
@@ -26,13 +26,22 @@ ui <- fluidPage(
                 tabPanel("Introduction", verbatimTextOutput("introduction"),
                          h4("Since the year 1970, Carbon Dioxide 
                                   emissions have increased 90%.(epa.gov)"),
+                         p("Carbon dioxide is measured by a sensor. Usually by
+                           an NDIR CO2 sensor. This sensor takes a sample of 
+                           infared light in an air sample and calculates the
+                           amount of CO2 molecules."),
                          p("In the most recently calculated year, 2021, the 
                            cumulative CO2 rate for the world was 1736930.125 kt. 
                            It is also important to understand each continents 
                            contribution towards CO2 emissions. Seeing each
                            the CO2 values for each country can help give us 
                            insight and enables us to dig deeper into the cause
-                           for these values."),
+                           for these values. We can analyze the top continents
+                           that produce the most CO2 and to bring awareness to
+                           the situation."),
+                         p("Being exposed to high levels of carbon dioxide can
+                           affect the health of humans, as well as the earth
+                           (climate change)."),
                          p("Africa: 49,133.684 kt"),
                          p("Antarctica: Insufficent Data"),
                          p("Asia: 557,596.562 kt"),
